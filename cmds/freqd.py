b7a00eec0dc6842b102266480dfceb3f9a2c6d1e
ddn <107157865+drowningnewt@users.noreply.github.com>
2024-03-14 10:36:00 -0400

    formatting

--- cmds/freqd.py
+++ cmds/freqd.py
@@ -6,14 +6,6 @@ from util import corpora, parser
 
 RESTRICTED = True
 
-def get_reverse(ngram):
-    return ngram[::-1]
-
-def calculate_freq(item, ngrams):
-    pattern = re.compile(item.replace('.', '\.').replace('_', '.'))
-    count = sum(value for key, value in ngrams.items() if pattern.search(key))
-    return count / sum(ngrams.values())
-
 def exec(message: Message):
     id = message.author.id
     query = parser.get_args(message)
@@ -21,10 +13,10 @@ def exec(message: Message):
     ntype = len(query[0]) if len(query) > 0 else None
 
     if not query or ntype != 2:
-        return "Please provide at least 1 ngram that is 2 char"
+        return "Please provide at least 1 bigram"
 
     if len(query) > 6:
-        return "Please provide no more than 6 ngrams"
+        return "Please provide no more than 6 bigrams"
 
     corpus = short_corpus(id, 7)
     start = f' {corpus.upper()}' + ' ' * (8 - len(corpus))
@@ -33,23 +25,23 @@ def exec(message: Message):
     processed_ngrams = set()
     freqs = []
 
-    intersection = f'{"-" * len(start)}{("-" * 8) * (len(corpora.NGRAMS) - 1)}'
+    dividers = f'{"-" * len(start)}{("-" * 8) * (len(corpora.NGRAMS) - 1)}'
 
     for item in query:
         if len(item) != ntype:
-            return 'All ngrams must be the same length'
+            return 'All ngrams must be bigrams'
 
         rvrs = get_reverse(item)
 
         if item in processed_ngrams or rvrs in processed_ngrams:
             continue
-        alt.append(intersection)
+        alt.append(dividers)
         if item != rvrs:
             alt.extend([f' {item} + {rvrs} ',
                         f'   {item}    ',
                         f'   {rvrs}    '])
         else: 
-            alt.append(f' {item}      ')
+            alt.append(f'   {item}    ')
 
         for i in range(len(corpora.NGRAMS) - 1):
             ngrams = corpora.ngrams(i + 2, id=id)
@@ -78,12 +70,19 @@ def exec(message: Message):
     if len(query) == 1 or all(item == query[0] or get_reverse(item) == query[0] for item in query):
         alt.append('```')
     elif len(query) > 1:
-        alt.extend([intersection, 
-                    f' total   '])
+        alt.extend([dividers, f' total   '])
         for i, freq in enumerate(freqs):
             alt[-1] += f'| {freq:.2%} '
         alt.append('```')
     return '\n'.join(alt)
+
+def get_reverse(ngram):
+    return ngram[::-1]
+
+def calculate_freq(item, ngrams):
+    pattern = re.compile(item.replace('.', '\.').replace('_', '.'))
+    count = sum(value for key, value in ngrams.items() if pattern.search(key))
+    return count / sum(ngrams.values())
 
 def short_corpus(id: int, length: int):
     name = corpora.get_corpus(id)
@@ -98,7 +97,7 @@ def short_corpus(id: int, length: int):
         name = "e" + name[len("english-"):]
     elif "-" in name:
         parts = name.split("-")
-        name = f"{parts[0][0]}-{parts[1][0]}"
+        name = f'{parts[0][0]}-{parts[1][0]}'
 
     if len(name) > length:
         name = name[:length]
