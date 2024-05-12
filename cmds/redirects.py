46c1bc4910b7b29cb3479a21bade05885fc990c2
ddn <107157865+drowningnewt@users.noreply.github.com>
2024-05-11 23:58:50 -0400

    add bad-redirects

--- cmds/redirects.py
+++ cmds/redirects.py
@@ -3,28 +3,54 @@ from discord import Message
 from util import corpora, memory, parser
 from util.analyzer import TABLE
 
+OUTPUT = 10
+
 def exec(message: Message):
-    name = parser.get_arg(message)
-    ll = memory.find(name.lower())
+    kwargs: dict[str, bool] = parser.get_kwargs(message, str, bad=bool)
+    layout_name = kwargs['args']
+    ll = memory.find(layout_name.lower())
+
+    if not layout_name:
+        tips = [
+            'Usage: `redirects [layout_name] [--arg]`',
+            "```",
+            "no arg:",
+            "    view all redirects of a layout",
+            "--bad:",
+            "    view bad redirects of a layout",
+            "```"
+        ]
+        return '\n'.join(tips)
+
+    arg = 'Redirect'
+    if kwargs['bad']:
+        arg = 'Bad-Redirect'
 
     trigrams = corpora.ngrams(3, id=message.author.id)
     total = sum(trigrams.values())
 
-    lines = []
+    red = {}
     for gram, count in trigrams.items():
         gram = gram.lower()
+
         if len(set(gram)) != len(gram): # ignore repeats
             continue
 
         key = '-'.join([ll.keys[x].finger for x in gram if x in ll.keys])
 
-        if key in TABLE and TABLE[key].lower().endswith('redirect'):
-            lines.append(f'{gram:<5} {count / total:.3%}')
+        if key in TABLE and TABLE[key].lower().endswith(arg.lower()):
+            red[gram] = red.get(gram, 0) + count
 
-    return '\n'.join(['```', f'Top 10 {ll.name} Redirects:'] + lines[:10] + ['```'])
+    g_total = sum(count for (_, count) in red.items())
+
+    res = []
+    for ngram, count in red.items():
+        res.append(f'{ngram:<6} {count / total:.3%}')
+
+    return '\n'.join(['```', f'Top {OUTPUT} {ll.name} {arg}s:'] + res[:OUTPUT] + [f'Total: {g_total / total:.3%}'] + ['```'])
 
 def use():
-    return 'redirects [layout name]'
+    return 'redirects [layout name] [--arg]'
 
 def desc():
     return 'see the worst redirects for a particular layout'
