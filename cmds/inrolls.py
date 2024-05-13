d45b72573cbbcadbe9436cd8f95156245be98a34
ddn <107157865+drowningnewt@users.noreply.github.com>
2024-05-12 22:00:36 -0400

    fix some things

--- cmds/inrolls.py
+++ cmds/inrolls.py
@@ -10,18 +10,26 @@ def exec(message: Message):
     trigrams = corpora.ngrams(3, id=message.author.id)
     total = sum(trigrams.values())
 
-    lines = []
+    inrolls = {}
     for gram, count in trigrams.items():
         gram = gram.lower()
+        
         if len(set(gram)) != len(gram): # ignore repeats
             continue
 
         key = '-'.join([ll.keys[x].finger for x in gram if x in ll.keys])
 
         if key in TABLE and TABLE[key].startswith('roll-in'):
-            lines.append(f'{gram:<5} {count / total:.3%}')
+            inrolls[gram] = inrolls.get(gram, 0) + count
 
-    return '\n'.join(['```', f'Top 10 {ll.name} Inrolls:'] + lines[:10] + ['```'])
+    g_total = sum(count for (_, count) in inrolls.items()) / total
+
+    res = []
+    format_len = f'{len(f"{g_total:.3%}")}'
+    for ngram, count in inrolls.items():
+        res.append(f'{ngram:<{format_len}} {count / total:.3%}')
+
+    return '\n'.join(['```', f'Top 10 {ll.name} Inrolls:'] + res[:10] + [f'Total: {g_total:.3%}'] + ['```'])
 
 def use():
     return 'inrolls [layout name]'
