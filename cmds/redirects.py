8a840d87b12b5b66d876b99d5125a5d9843a780a
ddn <107157865+drowningnewt@users.noreply.github.com>
2024-05-12 21:53:48 -0400

    add bad red

--- cmds/redirects.py
+++ cmds/redirects.py
@@ -1,30 +1,63 @@
-from discord import Message
+from discord import Message, ChannelType
 
 from util import corpora, memory, parser
 from util.analyzer import TABLE
 
 def exec(message: Message):
-    name = parser.get_arg(message)
-    ll = memory.find(name.lower())
+    kwargs: dict[str, bool] = parser.get_kwargs(message, str, len=list, bad=bool)
+    layout_name = kwargs['args']
+    ll = memory.find(layout_name.lower())
+    is_dm = message.channel.type == ChannelType.private
+
+    if not layout_name:
+        tips = [
+            'Usage: `redirects [layout_name] [--arg]`',
+            "```",
+            "no arg:",
+            "    view all redirects of a layout",
+            "--bad:",
+            "    view bad redirects of a layout",
+            "--len [int]:",
+            "    view set number of redirects in dms",
+            "```"
+        ]
+        return '\n'.join(tips)
+
+    arg = 'Redirect'
+    if kwargs['bad']:
+        arg = 'Bad-Redirect'
+
+    if kwargs['len'] and is_dm:
+        leng = int(kwargs['len'][0]) if int(kwargs['len'][0]) <= 100 else 100
+    else:
+        leng = 10
 
     trigrams = corpora.ngrams(3, id=message.author.id)
     total = sum(trigrams.values())
 
-    lines = []
+    red = {}
     for gram, count in trigrams.items():
         gram = gram.lower()
+
         if len(set(gram)) != len(gram): # ignore repeats
             continue
 
         key = '-'.join([ll.keys[x].finger for x in gram if x in ll.keys])
 
-        if key in TABLE and TABLE[key].lower().endswith('redirect'):
-            lines.append(f'{gram:<5} {count / total:.3%}')
+        if key in TABLE and TABLE[key].lower().endswith(arg.lower()):
+            red[gram] = red.get(gram, 0) + count
 
-    return '\n'.join(['```', f'Top 10 {ll.name} Redirects:'] + lines[:10] + ['```'])
+    g_total = sum(count for (_, count) in red.items()) / total
+
+    res = []
+    format_len = f'{len(f"{g_total:.3%}")}'
+    for ngram, count in red.items():
+        res.append(f'{ngram:<{format_len}} {count / total:.3%}')
+
+    return '\n'.join(['```', f'Top {len(res[:leng])} {ll.name} {arg}s:'] + res[:leng] + [f'Total: {g_total:.3%}'] + ['```'])
 
 def use():
-    return 'redirects [layout name]'
+    return 'redirects [layout name] [--arg]'
 
 def desc():
     return 'see the worst redirects for a particular layout'
